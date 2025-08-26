839c57ee04b26eff73f003ec20c081ae2e4737eb
Hirwa Arrnaud <hirwatrevor8@gmail.com>
2025-08-26 10:14:32 +0200

    added all mages and names

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,9 +3,9 @@ function Card () {
     return(
         <div className="holder">
         <div className="cards">
-        <img src="./2.jpg" alt="img" ></img>
-        <h2>Air Jordan 10</h2>
-        <p>37,000 RWF</p>
+        <img src="./1.avif" alt="img" ></img>
+        <h2>Air Jordan 1</h2>
+        <p>25,000 RWF</p>
         </div>
                 <div className="cards">
         <img src="./3.avif" alt="img" ></img>
@@ -14,33 +14,33 @@ function Card () {
         </div>
                 <div className="cards">
         <img src="./5.avif" alt="img" ></img>
-        <h2>Air Jordan 11</h2>
-        <p>35,000 RWF</p>
+        <h2>Air Jordan 5</h2>
+        <p>28,000 RWF</p>
         </div>
                 <div className="cards">
         <img src="./8.avif" alt="img" ></img>
-        <h2>Air Jordan 11</h2>
-        <p>35,000 RWF</p>
+        <h2>Air Jordan 9</h2>
+        <p>38,000 RWF</p>
         </div>
                 <div className="cards">
-        <img src="./5.avif" alt="img" ></img>
-        <h2>Air Jordan 11</h2>
-        <p>35,000 RWF</p>
+        <img src="./4.webp" alt="img" ></img>
+        <h2>Air Jordan 4</h2>
+        <p>27,000 RWF</p>
         </div>
                 <div className="cards">
         <img src="./13.avif" alt="img" ></img>
-        <h2>Air Jordan 11</h2>
-        <p>35,000 RWF</p>
+        <h2>Air Jordan 13</h2>
+        <p>32,500 RWF</p>
         </div>
                 <div className="cards">
-        <img src="./3.avif" alt="img" ></img>
-        <h2>Air Jordan 11</h2>
-        <p>35,000 RWF</p>
+        <img src="./cact.webp" alt="img" ></img>
+        <h2>Jack Cactus</h2>
+        <p>27,800 RWF</p>
         </div>
                 <div className="cards">
         <img src="./9.avif" alt="img" ></img>
-        <h2>Air Jordan 11</h2>
-        <p>35,000 RWF</p>
+        <h2>Air Jordan 12</h2>
+        <p>27,500 RWF</p>
         </div>
         <footer>
             <p> 2025 Nike all rights reserved</p>
